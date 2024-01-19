4eefb803b5f4ddfde7c8592e4ed7ef7b0d2b16a7
albint3r <albint3r@gmail.com>
2024-01-18 18:13:40 -0600

    Add steps logic in the form

--- lib/presentation/dashboard/widgets/body_dashboard.dart
+++ lib/presentation/dashboard/widgets/body_dashboard.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
 
 import '../../../aplication/dashboard/dashboard_bloc.dart';
 import '../../core/theme/const_values.dart';
+import '../../core/widgets/text/text_title.dart';
 import 'form_dialog_area/dialog_form.dart';
 import 'form_dialog_area/form_area.dart';
 import 'lateral_menu.dart';
@@ -40,8 +42,17 @@ class BodyDashBoard extends StatelessWidget {
           ],
         ),
         if (dashboard.showForm)
-          const DialogForm(
-            child: FormArea(),
+          DialogForm(
+            child: dashboard.isFinished
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const CircularProgressIndicator(),
+                      const Gap(10),
+                      TextTitle.h3('Creating ChatBot'),
+                    ],
+                  )
+                : const FormArea(),
           ),
       ],
     );
