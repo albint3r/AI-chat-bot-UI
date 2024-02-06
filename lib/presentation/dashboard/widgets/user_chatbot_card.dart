6496823a1914002d8c5fd05eb903258a017a33ab
albint3r <albint3r@gmail.com>
2024-02-06 10:39:20 -0600

    Add long press to navigate to the id chatbot

--- lib/presentation/dashboard/widgets/user_chatbot_card.dart
+++ lib/presentation/dashboard/widgets/user_chatbot_card.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 import '../../../domain/dashboard/user_chatbot.dart';
+import '../../core/router/app_router.dart';
 import '../../core/theme/const_values.dart';
 import 'is_active_chat_toggle_switch.dart';
 import 'single_chat_item.dart';
@@ -18,39 +20,46 @@ class UserChatBotCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
-    return Card(
-      color: colorScheme.onPrimary.withOpacity(.5),
-      child: Padding(
-        padding: const EdgeInsets.all(padding),
-        child: SizedBox(
-          height: elevatedButtonHeight,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              SingleChatItem(
-                'Name',
-                userChatBot.name,
-                const Icon(Icons.folder),
-              ),
-              SingleChatItem(
-                'Describe',
-                userChatBot.description ?? '',
-                const Icon(Icons.description),
-              ),
-              SingleChatItem(
-                'Index',
-                userChatBot.indexName,
-                const Icon(Icons.info),
-              ),
-              SingleChatItem(
-                'Questions',
-                userChatBot.totalQuestions.toString(),
-              ),
-              IsActiveChatToggleSwitch(
-                userChatBot,
-                index: index,
-              ),
-            ],
+    return InkWell(
+      onLongPress: () => context.router.push(
+        ChatBotRoute(
+          chatId: userChatBot.chatbotId,
+        ),
+      ),
+      child: Card(
+        color: colorScheme.onPrimary.withOpacity(.5),
+        child: Padding(
+          padding: const EdgeInsets.all(padding),
+          child: SizedBox(
+            height: elevatedButtonHeight,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SingleChatItem(
+                  'Name',
+                  userChatBot.name,
+                  const Icon(Icons.folder),
+                ),
+                SingleChatItem(
+                  'Describe',
+                  userChatBot.description ?? '',
+                  const Icon(Icons.description),
+                ),
+                SingleChatItem(
+                  'Index',
+                  userChatBot.indexName,
+                  const Icon(Icons.info),
+                ),
+                SingleChatItem(
+                  'Questions',
+                  userChatBot.totalQuestions.toString(),
+                ),
+                IsActiveChatToggleSwitch(
+                  userChatBot,
+                  index: index,
+                ),
+              ],
+            ),
           ),
         ),
       ),
